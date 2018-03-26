9a389b4d6b4a247e9913e0d1480fd3a005a54e38
Wouter Raateland <wouterraateland@gmail.com>
2018-03-26 22:57:04 +0200

    Fill in program

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import classNames from 'classnames'
 import './styles.css'
 
-const EventCard = ({ revealed = false, title, bgImage, bgColor }) => (
+const pad = c => n => s =>
+  c.repeat(n - s.toString().length) + s
+
+const zeroPad = pad('0')
+const zeroPad2 = zeroPad(2)
+const Time = ({ time }) => `${zeroPad2(time.getHours())}:${zeroPad2(time.getMinutes())}`
+
+const EventCard = ({ revealed = false, title, start, end, bgImage, bgColor }) => (
   <div
-    className={classNames('EventCard', { revealed })}
+    className={classNames('EventCard', 'row', { revealed })}
     style={{
       backgroundImage: `url(${bgImage})`,
       backgroundColor: bgColor ? bgColor : '#fff',
@@ -13,6 +20,9 @@ const EventCard = ({ revealed = false, title, bgImage, bgColor }) => (
     <h2 className="EventCard-title">{revealed
       ? title
       : 'To be Revealed'}</h2>
+    <div className="EventCard-time">
+      <Time time={start} /> - <Time time={end} />
+    </div>
   </div>
 )
 
