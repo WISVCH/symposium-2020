ad260cf49462c9d7ae9e30e10c7ca11cc26235c2
Wouter Raateland <wouterraateland@gmail.com>
2018-03-28 15:43:43 +0200

    Update text and images

--- src/components/Committee/index.js
+++ src/components/Committee/index.js
@@ -3,47 +3,47 @@ import './styles.css'
 
 import Person from 'components/Person'
 
-// import cj from 'assets/commissioners/cor-jan_heijlema.jpg'
-// import hoef from 'assets/commissioners/emiel_hoefkens.jpg'
-// import arie from 'assets/commissioners/arianne_schiereck.jpg'
-// import wouter from 'assets/commissioners/wouter_raateland.jpg'
-// import ilo from 'assets/commissioners/ilona_post.jpg'
-// import hillie from 'assets/commissioners/hilco_van_der_wilk.jpg'
-// import ireen from 'assets/commissioners/irene_vooijs.jpg'
+import cj from 'assets/commissioners/cor-jan_heijlema.jpg'
+import hoef from 'assets/commissioners/emiel_hoefkens.jpg'
+import arie from 'assets/commissioners/arianne_schiereck.jpg'
+import wouter from 'assets/commissioners/wouter_raateland.jpg'
+import ilo from 'assets/commissioners/ilona_post.jpg'
+import hillie from 'assets/commissioners/hilco_van_der_wilk.jpg'
+import ireen from 'assets/commissioners/irene_vooijs.jpg'
 
 const commissioners = [
   {
-    img: null, //cj,
+    img: cj,
     name: 'Cor-Jan Heijlema',
     title: 'Chairman',
   },
   {
-    img: null, //hoef,
+    img: hoef,
     name: 'Emiel Hoefkens',
     title: 'Secretary',
   },
   {
-    img: null, //arie,
+    img: arie,
     name: 'Arianne Schiereck',
     title: 'Treasurer',
   },
   {
-    img: null, //wouter,
+    img: wouter,
     name: 'Wouter Raateland',
     title: 'Commissioner promotion',
   },
   {
-    img: null, //ilo,
+    img: ilo,
     name: 'Ilona Post',
     title: 'Commissioner speakers',
   },
   {
-    img: null, //hillie,
+    img: hillie,
     name: 'Hilco van der Wilk',
     title: 'Commissioner speakers',
   },
   {
-    img: null, //ireen,
+    img: ireen,
     name: 'Irene Vooijs',
     title: 'Supervisor',
   },
--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -7,8 +7,8 @@ const Introduction = () => (
   <div className="Introduction shadow accent row">
     <h2>Introducing the symposium</h2>
     <p>
-      This October, we will present healthcare in the future.
-      During this day, you will be enlightened by the academics who are currently pushing the boundaries in healthcare technology, improving health, bit by bit.<br />
+      This October, we will present you healthcare in the future.
+      In a day filled with talks, discussions and fun, you will be enlightened by the academics who are currently pushing the boundaries in healthcare technology, improving health, bit by bit.<br />
       <br />
       Stay tuned for more information and we are happy to see you in October.<br />
       <br />
