4494392674b595c6ae5d3d81171d450154de8e25
Wouter Raateland <wouterraateland@gmail.com>
2018-04-05 14:21:13 +0200

    Remove links to non sympo fb page

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -7,7 +7,7 @@ const SpeakersPage = () => (
   <div className="SpeakersPage page">
     <Section wrapper>
       <h1>Speakers</h1>
-      <p>The speakers will be revealed here as well as on Facebook. Follow our <a href="https://facebook.com/sympo">Facebook page</a> or check back here to stay up to date.</p>
+      <p>The speakers will be revealed here as well as on Facebook. Follow our <a href="">Facebook page</a>(Comming soon) or check back here to stay up to date.</p>
     </Section>
     <Section wrapper>
       <Speakers />
