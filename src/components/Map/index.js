be25336e763447a7b80157ee94aa630a7e58c183
Wouter Raateland <wouterraateland@gmail.com>
2018-04-11 15:31:24 +0200

    Fix bug on safari

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -9,7 +9,7 @@ const Map = () => (
     <iframe
       className="Map"
       title="Map around Theater de Veste"
-      src="//www.google.com/maps/embed/v1/place?q=Theater+de+Veste,Delft
+      src="https://www.google.com/maps/embed/v1/place?q=Theater+de+Veste,Delft
       &zoom=17
       &key=AIzaSyD0jHEFwuRFjEevoPWQl871z89h_BzPT7I"
     />
