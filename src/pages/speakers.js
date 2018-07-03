2d5b136296c5b12fdb7023c5502dd21f37446e18
Wouter Raateland <wouterraateland@gmail.com>
2018-07-03 18:36:18 +0200

    Add facebook event links

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -7,7 +7,7 @@ const SpeakersPage = () => (
   <div className="SpeakersPage page">
     <Section wrapper>
       <h1>Speakers</h1>
-      <p>The speakers will be revealed here as well as on Facebook. Follow our <a href="">Facebook page</a>(Comming soon) or check back here to stay up to date.</p>
+      <p>The speakers will be revealed here as well as on Facebook. Follow our <a target="_blank" href="https://www.facebook.com/events/165194104170772/" rel="noopener noreferrer">Facebook page</a> or check back here to stay up to date.</p>
     </Section>
     <Section wrapper>
       <Speakers />
