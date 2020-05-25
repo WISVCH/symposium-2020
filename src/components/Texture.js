8e15a633985b7eedb8582f459aa7cdd665b0a47c
Kawin Zheng <k.zheng-2@student.tudelft.nl>
2020-05-25 16:22:59 +0200

    wave as background

--- src/components/Texture.js
+++ src/components/Texture.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
-import texture from 'assets/texture.svg'
+import texture from 'assets/golftransparant.svg'
+// import texture from 'assets/texture.svg'
 
 const Texture = styled.div`
   pointer-events: none;
   position: absolute;
-  background: url(${texture}) repeat center / 6em;
+  background: url(${texture}) repeat center;
   mask-image: radial-gradient(ellipse 50% 50% at 50% 50%, rgba(0, 0, 0, 1), transparent);
 `
 
