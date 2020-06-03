59ac3c8caaf1d7778ecf8362aaee836a9a445c63
Kawin Zheng <k.zheng-2@student.tudelft.nl>
2020-06-03 10:09:41 +0200

    try extra fix

--- src/components/Texture.js
+++ src/components/Texture.js
@@ -1,13 +1,12 @@
 import styled from 'styled-components'
 
 import texture from 'assets/Golf_transparant_wit.svg'
-// import texture from 'assets/golftransparant.svg'
 // import texture from 'assets/texture.svg'
 
 const Texture = styled.div`
   pointer-events: none;
   position: absolute;
-  background: url(${texture}) center;
+  background: url(${texture});
   mask-image: radial-gradient(ellipse 50% 50% at 50% 50%, rgba(0, 0, 0, 1), transparent);
 `
 
