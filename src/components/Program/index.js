756d7a50c658cd4aea03bbbc6ac116e17864aa1c
Kawin Zheng <k.zheng-2@student.tudelft.nl>
2020-08-26 00:00:06 +0200

    if event or speaker is not revealed show TBA event

--- src/components/Program/index.js
+++ src/components/Program/index.js
@@ -4,12 +4,24 @@ import { H2 } from 'components/lib'
 import Event from './Event'
 
 import events from './data'
+import leenstra from "../../assets/speakers/b_leenstra.jpg";
 
 export default () => (
   <div>
-    <H2>22 October 2018</H2>
+    <H2>19 November 2020</H2>
     {events.map((event, i) =>
-      <Event key={i} {...event} />
+        {
+            if (event.revealed) {
+                return <Event key={i} {...event} />
+            } else {
+                const emptyEvent = event;
+                emptyEvent.title = "Soon to be revealed";
+                emptyEvent.meta = "";
+                emptyEvent.speaker = "";
+                emptyEvent.event = "";
+                return <Event key={i} {...emptyEvent} />
+            }
+        }
     )}
   </div>
 )
