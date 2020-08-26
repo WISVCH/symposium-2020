94f1724b672291c3f94ff220936d0726c17e6e2b
Daniel van Gelder <danielvangelder@MacBook-Pro.local>
2020-08-26 10:48:40 +0200

    activate program page

--- src/components/Program/index.js
+++ src/components/Program/index.js
@@ -7,7 +7,7 @@ import events from './data'
 
 export default () => (
   <div>
-    <H2>22 October 2018</H2>
+      {/*<H2>22 October 2018</H2>*/}
     {events.map((event, i) =>
       <Event key={i} {...event} />
     )}
