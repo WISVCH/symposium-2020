f2a695f2012e37bf6409ac27e8da1b296f7650f5
Daniel van Gelder <danielvangelder@MacBook-Pro.local>
2020-10-13 10:34:29 +0200

    add registration link

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -13,10 +13,10 @@ export default () => (
     <Route path="/" exact component={HomePage} />
     <Route path="/program" component={ProgramPage} />
     <Route path="/event/:event" component={EventPage} />
-    {<Route path="/about" component={AboutPage} />}
+    <Route path="/about" component={AboutPage} />
     <Route path="/tickets" component={TicketsPage} />
-    {/*<Route path="/tickets" component={() =>*/}
-      {/*window.location.replace(`https://ch.tudelft.nl/events/e6ad556f-4fd2-4fb5-b343-32651a1aca40/`)} />*/}
+    <Route path="/register" component={() =>
+      window.location.replace("http://wisv.ch/sympoticket")} />
     {/*<Route path='/history' component={() =>*/}
       {/*window.location.replace(`https://symposium.ch.tudelft.nl/2017/`)} />*/}
     <Route component={NotFoundPage} />
