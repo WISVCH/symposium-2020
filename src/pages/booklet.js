16ad7ab8c819b797b829b04c46a911a56cb2817e
Daniël van Gelder <dvangelder4@gmail.com>
2020-12-16 14:37:12 +0100

    page number to text html

--- src/pages/booklet.js
+++ src/pages/booklet.js
@@ -36,7 +36,7 @@ export default class Booklet extends Component {
                     >
                         <Page pageNumber={pageNumber} width={600} />
                     </Document>
-                    Page {pageNumber} of {numPages}
+                    <Text>Page {pageNumber} of {numPages}</Text>
                 </Section>
             // </Page>
         )
