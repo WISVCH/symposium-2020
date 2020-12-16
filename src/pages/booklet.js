8aba60dc9724afedecd03b963897637b13845df8
Kawin Zheng <k.zheng-2@student.tudelft.nl>
2020-12-16 01:51:58 +0100

    add pdf viewer

--- src/pages/booklet.js
+++ src/pages/booklet.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-import {Page, H1, H2, Text, Button} from 'components/lib'
+import {H1, H2, Text, Button} from 'components/lib'
 import Section from 'components/Section'
-import Tickets from 'components/Tickets'
+import booklet from 'assets/Symposium_Advanced_Analytics_Booklet.pdf'
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default () => (
-  <Page>
-    <Section>
-      <H1>Symposium Advanced Analytics Booklet</H1>
+export default class Booklet extends Component {
+    state = { numPages: null, pageNumber: 1 };
 
-    </Section>
-  </Page>
-)
+    onDocumentLoadSuccess = ({ numPages }) => {
+        this.setState({ numPages });
+    };
+
+    goToPrevPage = () =>
+        this.state.pageNumber > 1 ? this.setState(state => ({ pageNumber: state.pageNumber - 1 })) : this.setState(state => ({ pageNumber: state.pageNumber }));
+    goToNextPage = () =>
+        this.state.pageNumber < this.state.numPages ? this.setState(state => ({ pageNumber: state.pageNumber + 1 })) : this.setState(state => ({ pageNumber: state.pageNumber }));
+
+
+    render() {
+        const { pageNumber, numPages } = this.state;
+
+        return (
+            // <Page>
+                <Section>
+                    <H1>Symposium Advanced Analytics Booklet</H1>
+                    <nav>
+                        <button onClick={this.goToPrevPage}>Prev</button>
+                        <button onClick={this.goToNextPage}>Next</button>
+                    </nav>
+                    <Document
+                        file={booklet}
+                        onLoadSuccess={this.onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={pageNumber} width={600} />
+                    </Document>
+                    Page {pageNumber} of {numPages}
+                </Section>
+            // </Page>
+        )
+    }
+}
